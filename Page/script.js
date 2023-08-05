04a821957353cfbedb2767a58f6dd2ef71f1e234
sengupta03 <senguptarakesh3@gmail.com>
2023-08-05 22:45:40 +0530

    doctor

---
+++ Page/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
